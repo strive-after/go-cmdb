0bade5f42c570dfccfefe62719f6e40419ce95a0
屈帅波 <qushuaibo@chicv.com>
2020-07-13 10:06:04 +0800

    update main.go

--- module/crud.go
+++ module/crud.go
@@ -1,5 +1,12 @@
 package module
 
+import (
+	"encoding/gob"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"github.com/strive-after/go-cmdb/util"
+)
+
 const  (
 	userTableName string = "user"
 	groupTableName string = "group"
@@ -9,6 +16,24 @@ var (
 	ok bool
 	err       error
 )
+
+func init() {
+	//未注册  必须注册才能用 有时候session获取的时候  它会提示  gob: name not registered for interface: "github.com/strive-after/go-kubernetes/module.User"
+	//那么就注册一下
+	//因为session是gob方式加载结构体等就需要注册
+	gob.Register(User{})
+	//beego.BConfig.WebConfig.Session.SessionProvider = "redis"
+	//beego.BConfig.WebConfig.Session.SessionProviderConfig = beego.AppConfig.String("redisurl")
+	beego.AddFuncMap("TimeForMat",util.StopTimeFormat)
+	beego.AddFuncMap("Roles",util.Role)
+	beego.AddFuncMap("Left",util.IndexLeft)
+	beego.AddFuncMap("Right",util.IndexRight)
+	err := logs.SetLogger(logs.AdapterMultiFile, `{"filename":"logs/test.log","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)  //separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)
+	if err != nil {
+		beego.Error(err)
+		return
+	}
+}
 
 
 type Operation interface {
